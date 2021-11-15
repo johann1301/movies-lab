f2aea0957b4a9ee6961493ee9e4ee4236f2200e2
Johannes von Kessel <johannes.v.kessel@web.de>
2021-11-15 18:06:10 +0100

    almost done

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -8,10 +8,80 @@ router.get('/celebrities', (req, res, next) => {
 		.then(celebritiesFromDB => {
 			console.log(celebritiesFromDB)
 			
-			res.render('../views/Celebrities/index.hbs', { celebrityList: celebritiesFromDB })
+			res.render('../views/celebrities/index.hbs', { celebrityList: celebritiesFromDB })
 		})
 		.catch(err => next(err))
 	
 });
+
+router.get('/celebrities/new', (req, res, next) => {
+	res.render('celebrities/new')
+});
+
+
+
+
+router.post('/celebrities', (req, res, next) => {
+    const { name, occupation, catchPhrase} = req.body
+   console.log(req.body)
+   Celebrity.create({
+    name: name,
+    occupation: occupation,
+    catchPhrase: catchPhrase,
+})
+    .then(createdCelebrity => {
+        console.log(createdCelebrity)
+        
+        res.redirect(`/celebrities/${createdCelebrity._id}`)
+    })
+    .catch(err => next(err))
+})
+
+router.get('/celebrities/:id', (req, res, next) => {
+	const id = req.params.id
+	Celebrity.findById(id)
+		.then(celebrityFromDB => {
+			console.log(celebrityFromDB)
+			res.render('celebrities/show', { celebrity: celebrityFromDB })
+		})
+		.catch(err => next(err))
+});
+
+router.post('/celebrities/:id/delete', (req, res, next) => {
+    const id = req.params.id
+    Celebrity.findByIdAndDelete(id)
+    .then(() => {
+        
+        res.redirect('/celebrities')
+    })
+    .catch(err => {
+        next(err)
+    })
+
+    router.get('/celebrities/:id/edit', (req, res, next) => {
+        const id = req.params.id
+        Celebrity.findById(id)
+            .then(celebrityFromDB => {
+                res.render('celebrities/edit', { celebrity: celebrityFromDB })
+            })
+            .catch(err => next(err))
+    });
+
+    router.post('/celebrities/:id/edit', (req, res, next) => {
+        const { name, occupation, catchPhrase} = req.body
+        Celebrity.update({
+            name: name,
+            occupation: occupation,
+            catchPhrase: catchPhrase,
+        })
+            .then(updateCelebrity => {
+                console.log(updateCelebrity)
+                
+                res.redirect(`/celebrities`)
+            })
+            .catch(err => next(err))
+        })
+
+})
 
 module.exports = router;
